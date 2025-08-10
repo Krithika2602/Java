ca273b9ea835a6872bb8ac81a707fb9d67b15053
Krithika2602 <krithika2602@gmail.com>
2025-08-10 19:23:32 +0530

    Add files via upload

---
+++ Day2/SwapTemp.java
@@ -0,0 +1,22 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class SwapTemp {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		int temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("After swapping: a = " + a + ", b = " + b);
+		
+
+	}
+
+}
